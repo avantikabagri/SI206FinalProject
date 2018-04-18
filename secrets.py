7ededa7779fe52580bacaad4ef0f942b497a4fb5
avantikabagri <abagri@umich.edu>
2018-04-18 00:17:27 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
 import plotly
 
-NEWS_API_KEY = '3315cb61c50e4b5e8038208cccb25e9d'
-plotly.tools.set_credentials_file(username = 'abagri', api_key = 'T31XQF1Aw1BgvFep1CSU')
+NEWS_API_KEY = ''
+plotly.tools.set_credentials_file(username = '', api_key = '')
